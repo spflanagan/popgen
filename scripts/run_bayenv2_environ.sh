606ea3aaa9fc68b9be2acce0761d83c60a885efb
spflanagan <sflanagan19@gmail.com>
2016-03-19 10:47:28 -0500

    update

--- scripts/run_bayenv2_environ.sh
+++ scripts/run_bayenv2_environ.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #run from scripts
-cd ../sw_results/bayenv2
+cd ../sw_results/environmental_assoc/new_bayenv/
 for filename in ./pruned_snps/*; do
 	echo "Running bayenv2 on $filename"
-	~/Programs/bayenv_2/bayenv2 -i $filename -m matrix.1.out \
-		-e env_data_bayenv.txt -p 12 -k 100000 -n 5 -t -c -X -f \
-		-r 628398 -o ./pruned_snps/$filename.freq
+	~/Programs/bayenv2/bayenv2 -i $filename -m matrix.txt \
+		-e standardized.env -p 12 -k 100000 -n 5 -t -c -X -f \
+		-r 628398 -o $filename.freq
 done
