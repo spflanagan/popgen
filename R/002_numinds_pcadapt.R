0c0c48191ee35cba83f485a2184f363dadc1ac07
spflanagan <sflanagan19@gmail.com>
2018-12-02 08:52:51 +1300

    running pcadapt with different numbers of individuals

--- R/002_numinds_pcadapt.R
+++ R/002_numinds_pcadapt.R
@@ -6,20 +6,25 @@ setwd("Research/popgen/fwsw_results/")
 
 ped<-read.delim("subset.ped",header=FALSE,sep=" ", stringsAsFactors = FALSE)
 
-ns<-c(10,20,30)
+ns<-c(10,20,30,0)
 
 pcadapt_subset<-function(ped, num_inds){
-  keepinds<-unlist(tapply(ped[,2],ped[,1],sample,size=num_inds,replace=FALSE))
+  if(num_inds==0){
+    keepinds<-ped[,2]
+  }else{
+    keepinds<-unlist(tapply(ped[,2],ped[,1],sample,size=num_inds,replace=FALSE))
+  }
   pedsub<-ped[ped[,2] %in% keepinds,]
   subname<-paste("subset",num_inds,"inds.ped",sep="")
   write.table(pedsub,subname,col.names = FALSE,row.names = FALSE,quote=FALSE,sep=" ")
   filename<-read.pcadapt(subname,type="ped")
-  x<-pcadapt(filename, K=6)
+  x<-pcadapt(filename,K=20)
   saveRDS(x,paste("subset",num_inds,"inds.RDS",sep=""))
-  plot(x,option="screeplot")#K=6
+  png(paste("subset",num_inds,"inds.png"),height=7,width=7,units="in",res=300)
+  plot(x,option="scores",pop=pedsub[,1])#K=6
+  dev.off()
   return(x)
 }
 
-par(mfrow=c(2,2))
 pcasubs<-lapply(ns,pcadapt_subset,ped = ped)
 
