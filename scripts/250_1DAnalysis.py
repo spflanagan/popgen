7d0507ff2c7795e1f1d7e799fc36709d261698a6
spflanagan <spflanagan.phd@gmail.com>
2019-01-11 16:46:37 +1300

    finishing dadi 1D analysis

--- scripts/250_1DAnalysis.py
+++ scripts/250_1DAnalysis.py
@@ -69,72 +69,148 @@ for i in range(len(pops)):
 	pts = projs[i]+10
 
 	p_labels = "nu, T"
-	Optimize_Routine(fs, pts, pops[i], "two_epoch", dadi.Demographics1D.two_epoch, 5,2, fs_folded=True, param_labels = p_labels)
-	Optimize_Routine(fs, pts, pops[i], "growth", dadi.Demographics1D.growth, 5,2, fs_folded=True, param_labels = p_labels)
+	Optimize_Routine(fs, pts, pops[i], "two_epoch", dadi.Demographics1D.two_epoch, rounds=5,param_number=2, fs_folded=True, param_labels = "nu, T")
+	Optimize_Routine(fs, pts, pops[i], "growth", dadi.Demographics1D.growth, rounds=5,param_number=2, fs_folded=True, param_labels = "nu, T")
 	p_labels = "nuB, nuF, T"
-	Optimize_Routine(fs, pts, pops[i], "bottlegrowth", dadi.Demographics1D.bottlegrowth, 5,3, fs_folded=True, param_labels = p_labels)
+	Optimize_Routine(fs, pts, pops[i], "bottlegrowth", dadi.Demographics1D.bottlegrowth, rounds=5,param_number=3, fs_folded=True, param_labels = "nuB, nuF, T")
 	p_labels = "nuB, nuF, TB, TF"
-	Optimize_Routine(fs, pts, pops[i], "bottlegrowth", dadi.Demographics1D.three_epoch, 5,4, fs_folded=True, param_labels = p_labels)
+	Optimize_Routine(fs, pts, pops[i], "three_epoch", dadi.Demographics1D.three_epoch, rounds=5,param_number=4, fs_folded=True, param_labels = "nuB, nuF, TB, TF")
 	os.chdir("../")
 
 
 
-#This seems to work. Now how do I get the best fit parameters?
+#This seems to work. Extracted best fits in R
+
+#=================================================================================================#
+#									COMPARE MODELS TO DATA										  #
+#=================================================================================================#
+import pylab
+
+#pops = ['FLLG', 'FLCC', 'ALFW','ALST','LAFW','TXFW','TXCC']
+#projs = [70,      61,     72,     70,    72,    46,     61]
+
+#----------------------------------------------ALFW-----------------------------------------------#
+pts = 80
+nuB=0.0135
+nuF=10.5357
+T=0.2692
+alfw_bottle = dadi.Demographics1D.bottlegrowth(params = (nuB,nuF,T), ns=[72], pts = pts)
+dadi.Plotting.plot_1d_comp_multinom(alfw_bottle,alfw)
+
+#----------------------------------------------ALST-----------------------------------------------#
+pts = 80
+nuB=10.7382
+nuF=28.716
+T=0.9324
+alst_bottle = dadi.Demographics1D.bottlegrowth(params = (nuB,nuF,T), ns=[70], pts = pts)
+dadi.Plotting.plot_1d_comp_multinom(alst_bottle,alst)
+
+#----------------------------------------------FLCC-----------------------------------------------#
+pts = 80
+nu=29.7
+T=0.0881
+flcc_twoep = dadi.Demographics1D.two_epoch(params = (nu,T), ns=[61], pts = pts)
+dadi.Plotting.plot_1d_comp_multinom(flcc_twoep,flcc)
+
+pts = 80
+nu=29.0771
+T=0.119
+flcc_growth = dadi.Demographics1D.growth(params = (nu,T), ns=[61], pts = pts)
+dadi.Plotting.plot_1d_comp_multinom(flcc_growth,flcc)
+
+#----------------------------------------------FLLG-----------------------------------------------#
+pts = 80
+nuB=0.0114
+nuF=5.3033
+T=0.0586
+fllg_bottle = dadi.Demographics1D.bottlegrowth(params = (nuB,nuF,T), ns=[70], pts = pts)
+dadi.Plotting.plot_1d_comp_multinom(fllg_bottle,fllg)
+
+#----------------------------------------------LAFW-----------------------------------------------#
+pts = 80
+nuB=0.0368
+nuF=20.0107
+T=0.5108
+lafw_bottle = dadi.Demographics1D.bottlegrowth(params = (nuB,nuF,T), ns=[72], pts = pts)
+dadi.Plotting.plot_1d_comp_multinom(lafw_bottle,lafw)
+
+#----------------------------------------------TXCC-----------------------------------------------#
+pts = 80
+nu=16.3039
+T=1.1496
+txcc_growth = dadi.Demographics1D.growth(params = (nu,T), ns=[61], pts = pts)
+dadi.Plotting.plot_1d_comp_multinom(txcc_growth,txcc)
+
+#----------------------------------------------TXFW-----------------------------------------------#
+pts = 80
+nu=0.602
+T=0.0198
+txfw_growth = dadi.Demographics1D.growth(params = (nu,T), ns=[46], pts = pts)
+dadi.Plotting.plot_1d_comp_multinom(txfw_growth,txfw)
+
+pts = 80
+nu=0.5634
+T=0.0319
+txfw_twoep = dadi.Demographics1D.two_epoch(params = (nu,T), ns=[46], pts = pts)
+dadi.Plotting.plot_1d_comp_multinom(txfw_twoep,txfw)
+
+
+
 
 
 
 #=================================================================================================#
 #								ONE AT A TIME THE EXPLORATORY WAY 								  #
 #=================================================================================================#
-# set parameters 
-nu = 0.5 #ratio of contemporary to ancient population size
-T = 500 #2NA generations
-nuB = 0.1 #ratio of pop size after instantaneous change to ancient pop size
-nuF = 0.7 #ratio of pop size now to ancient pop size
-TB = 100 #length of bottleneck in 2Na generations
-TF = 100 #length of time since bottleneck recovery
+# # set parameters 
+# nu = 0.5 #ratio of contemporary to ancient population size
+# T = 500 #2NA generations
+# nuB = 0.1 #ratio of pop size after instantaneous change to ancient pop size
+# nuF = 0.7 #ratio of pop size now to ancient pop size
+# TB = 100 #length of bottleneck in 2Na generations
+# TF = 100 #length of time since bottleneck recovery
 
-# Single population models
+# # Single population models
 
-snm =  dadi.Demographics1D.snm(fllg,ns = ns,pts= 60)
+# snm =  dadi.Demographics1D.snm(fllg,ns = ns,pts= 60)
 
-two = dadi.Demographics1D.two_epoch(params = (nu,T), ns = ns, pts = 60)
+# two = dadi.Demographics1D.two_epoch(params = (nu,T), ns = ns, pts = 60)
 
-growth = dadi.Demographics1D.growth(params = (nu,T), ns = ns, pts = 60)
+# growth = dadi.Demographics1D.growth(params = (nu,T), ns = ns, pts = 60)
 
-bottle = dadi.Demographics1D.bottlegrowth(params = (nuB,nuF,T), ns = ns, pts = 60)
+# bottle = dadi.Demographics1D.bottlegrowth(params = (nuB,nuF,T), ns = ns, pts = 60)
 
-three = dadi.Demographics1D.three_epoch(params = (nuB,nuF,TB,TF), ns = ns, pts = 60)
+# three = dadi.Demographics1D.three_epoch(params = (nuB,nuF,TB,TF), ns = ns, pts = 60)
 
-# Compare models to data
-
-
-
-import pylab
-dadi.Plotting.plot_1d_comp_multinom(snm,fllg)
-dadi.Plotting.plot_1d_comp_Poisson(snm,fllg)
-
-dadi.Plotting.plot_1d_comp_multinom(two,fllg)
-
-dadi.Plotting.plot_1d_comp_multinom(growth,fllg)
-
-dadi.Plotting.plot_1d_comp_multinom(bottle,fllg)
-
-dadi.Plotting.plot_1d_comp_multinom(three,fllg)
+# # Compare models to data
 
 
-### I think I need to optimize parameters before continuing.
+
+# import pylab
+# dadi.Plotting.plot_1d_comp_multinom(snm,fllg)
+# dadi.Plotting.plot_1d_comp_Poisson(snm,fllg)
+
+# dadi.Plotting.plot_1d_comp_multinom(two,fllg)
+
+# dadi.Plotting.plot_1d_comp_multinom(growth,fllg)
+
+# dadi.Plotting.plot_1d_comp_multinom(bottle,fllg)
+
+# dadi.Plotting.plot_1d_comp_multinom(three,fllg)
 
 
-prefix = "FLLG1_1"
-pts = 60
+# ### I think I need to optimize parameters before continuing.
 
-p_labels = "ns, pts"
 
-Optimize_Routine(fllg, 60, prefix, "snm", dadi.Demographics1D.snm, 3,2, fs_folded=True)
+# prefix = "FLLG1_1"
+# pts = 60
 
-#or with a loop
-for i in range(1,6):
-    prefix = "FLLG1_1_Number_{}".format(i)
-    Optimize_Routine(fllg, pts, prefix, "snm", dadi.Demographics1D.snm, 3, 2, fs_folded=True, param_labels = p_labels)
+# p_labels = "ns, pts"
+
+# Optimize_Routine(fllg, 60, prefix, "snm", dadi.Demographics1D.snm, 3,2, fs_folded=True)
+
+# #or with a loop
+# for i in range(1,6):
+#     prefix = "FLLG1_1_Number_{}".format(i)
+#     Optimize_Routine(fllg, pts, prefix, "snm", dadi.Demographics1D.snm, 3, 2, fs_folded=True, param_labels = p_labels)
 
