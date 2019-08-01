dfea1cef46bb097bc1685b872b5de4cb75616aee
spflanagan <spflanagan.phd@gmail.com>
2019-08-01 15:32:25 +1200

    debugging ms

--- scripts/257_debug_ms.py
+++ scripts/257_debug_ms.py
@@ -38,7 +38,7 @@ nu2 = 8.4225
 m12 = 0.7777
 m21 = 0.0561
 T = 0.1441
-ns = [46,60]
+ns = [70,60]
 pts = 300
 
 asym_mig_fs = asym_mig(params=(nu1,nu2,m12,m21,T),ns=ns,pts=pts)
@@ -46,10 +46,50 @@ asym_mig_folded = dadi.Spectrum.fold(asym_mig_fs)
 dadi.Plotting.plot_single_2d_sfs(asym_mig_folded,vmin=0.000001)
 
 core="-m 1 2 1.5554 -m 2 1 0.1122 -n 1 0.1317 -n 2 8.4225 -ej 0.07205 2 1"
-command=dadi.Misc.ms_command(theta=0.1026112, ns=(60,70), core=core, iter=12103)
+command=dadi.Misc.ms_command(theta=0.1026112, ns=(70,60), core=core, iter=12103)
 ms_fs=dadi.Spectrum.from_ms_file(os.popen(command))
 folded_sfs=dadi.Spectrum.fold(ms_fs)
 dadi.Plotting.plot_single_2d_sfs(folded_sfs,vmin=0.000001)
+
+# compare between spectra
+
+# Data
+dd = dadi.Misc.make_data_dict ( "fwsw75.dadi.snps" )
+fl = dadi.Spectrum.from_data_dict(dd , pop_ids =[ 'FLLG','FLCC' ],projections =[70,60] ,polarized = False )
+pts = [ 220,240 ]
+
+#Provide best optimized parameter set for empirical data.
+#These will come from previous analyses you have already completed (above)
+emp_params = [0.1317,8.4225,0.7777,0.0561,0.1441]
+
+#Indicate whether your frequency spectrum object is folded (True) or unfolded (False)
+fs_folded = True
+#upper_bound=[15,15,10,10,10]
+#lower_bound=[.1,.1,0.01,0.01,0.01]
+#pmodel0 = dadi.Misc.perturb_params(emp_params, fold=1, upper_bound=upper_bound, lower_bound=lower_bound)
+#popt = dadi.Inference.optimize(pmodel0, fl, asym_mig, pts, lower_bound=lower_bound, upper_bound=upper_bound, maxiter=1000)
+#model = func_ex(popt, ns, pts_l)
+
+scaled_fl = Optimize_Empirical(fl, pts, "Empirical", "asym_mig", asym_mig, emp_params, fs_folded=fs_folded)
+
+dadi.Plotting.plot_2d_comp_Poisson(data=scaled_fl,model=asym_mig_folded,vmin=0.00001)
+dadi.Plotting.plot_2d_comp_Poisson(data=scaled_fl,model=folded_sfs,vmin=0.00001)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
