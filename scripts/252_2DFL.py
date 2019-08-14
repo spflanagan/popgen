78fdde1ae9c7c60bd9d9424bd50b83d0c4ce85fd
spflanagan <spflanagan@users.noreply.github.com>
2019-08-14 16:11:41 +1200

    heterogeneous models for FL

--- scripts/252_2DFL.py
+++ scripts/252_2DFL.py
@@ -18,11 +18,17 @@ from datetime import datetime
 execfile("../../programs/dadi_pipeline-master/Two_Population_Pipeline/Optimize_Functions.py")
 execfile("../../programs/dadi_pipeline-master/Two_Population_Pipeline/Models_2D.py")
 execfile("../../scripts/250_custom_dadi_models.py")
+execfile("../../scripts/dadi_selection_mods.py")
 #%load ../../programs/dadi_pipeline-master/Two_Population_Pipeline/Optimize_Functions.py
 #%load ../../programs/dadi_pipeline-master/Two_Population_Pipeline/Models_2D.py
 #%load ../../scripts/250_custom_dadi_models.py
 
+#### SET THESE ####
+plot=False
+homogeneous=False
+heterogeneous=True
 
+######################
 # Load the data
 dd = dadi.Misc.make_data_dict ( "fwsw75.dadi.snps" )
 #projections is sample size of alleles
@@ -38,62 +44,90 @@ os.chdir("FL2D")
 #=================================================================================================#
 #										PLOT SPECTRA	 										  #
 #=================================================================================================#
-
-#dadi.Plotting.plot_single_2d_sfs(fl,vmin=0.01)
+if plot is True:
+	dadi.Plotting.plot_single_2d_sfs(fl,vmin=0.01)
 
 
 #=================================================================================================#
 #										LOOP TO OPTIMIZE 										  #
 #=================================================================================================#
-pts = [ 220,240 ]
-rounds=4
-#define the lists for optional arguments
-#you can change these to alter the settings of the optimization routine
-reps = [10,20,30,40]
-maxiters = [3,5,10,15]
-folds = [3,2,2,1]
-fs_folded = True
-prefix = "fl"
+if homogeneous is True:
+	pts = [ 220,240 ]
+	rounds=4
+	#define the lists for optional arguments
+	#you can change these to alter the settings of the optimization routine
+	reps = [10,20,30,40]
+	maxiters = [3,5,10,15]
+	folds = [3,2,2,1]
+	fs_folded = True
+	prefix = "fl"
 
-for i in range(4,6):
-	prefix = "V1_Number_{}".format(i)
+	for i in range(4,6):
+		prefix = "V1_Number_{}".format(i)
+			
+		# Split into two populations, no migration.
+		Optimize_Routine(fl, pts, prefix, "no_mig", no_mig, rounds, 3, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, T",in_params=[0.16,9.79,0.13],in_upper=[15,10,10],in_lower=[0.1,0.1,0.01])
+
+		# Split into two populations, with continuous symmetric migration.
+		Optimize_Routine(fl, pts, prefix, "sym_mig", sym_mig, rounds, 4, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, m, T",in_params=[0.18,5.98,0.33,0.18],in_upper=[15,15,5,10],in_lower=[0.1,1,0.01,0.01])
 		
-	# Split into two populations, no migration.
-	Optimize_Routine(fl, pts, prefix, "no_mig", no_mig, rounds, 3, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, T",in_params=[0.16,9.79,0.13],in_upper=[15,10,10],in_lower=[0.1,0.1,0.01])
+		# Split into two populations, with continuous asymmetric migration.
+		Optimize_Routine(fl, pts, prefix, "asym_mig", asym_mig, rounds, 5, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, m12, m21, T",in_params=[0.15,9.39,0.44,0.06,0.15],in_upper=[15,15,10,10,10],in_lower=[.1,.1,0.01,0.01,0.01])
 
-	# Split into two populations, with continuous symmetric migration.
-	Optimize_Routine(fl, pts, prefix, "sym_mig", sym_mig, rounds, 4, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, m, T",in_params=[0.18,5.98,0.33,0.18],in_upper=[15,15,5,10],in_lower=[0.1,1,0.01,0.01])
-	
-	# Split into two populations, with continuous asymmetric migration.
-	Optimize_Routine(fl, pts, prefix, "asym_mig", asym_mig, rounds, 5, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, m12, m21, T",in_params=[0.15,9.39,0.44,0.06,0.15],in_upper=[15,15,10,10,10],in_lower=[.1,.1,0.01,0.01,0.01])
+		# Split into two pops, isolation with migration model
+		Optimize_Routine_Extrap(fl, pts, prefix, "IM", dadi.Demographics2D.IM, rounds, 6, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "s, nu1, nu2, T, m12, m21",in_params=[1.83,0.41,5.36,1.01,0.16,5.05],in_upper=[10,15,15,10,10,10],in_lower=[0.01,0.1,.1,.1,0.01,0.01])
+		
+		# Split into two pops, growth in one pop and two epoch in another
+		Optimize_Routine(fl, pts, prefix, "growth_twoep_no", growth_twoep_no_mig, rounds, 4, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2,T,Tc",in_params=[1.01,4.99,0.55,0.74],in_upper=[15,5,10,1],in_lower=[1,1,0.01,0.01])
 
-	# Split into two pops, isolation with migration model
-	Optimize_Routine_Extrap(fl, pts, prefix, "IM", dadi.Demographics2D.IM, rounds, 6, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "s, nu1, nu2, T, m12, m21",in_params=[1.83,0.41,5.36,1.01,0.16,5.05],in_upper=[10,15,15,10,10,10],in_lower=[0.01,0.1,.1,.1,0.01,0.01])
-	
-	# Split into two pops, growth in one pop and two epoch in another
-	Optimize_Routine(fl, pts, prefix, "growth_twoep_no", growth_twoep_no_mig, rounds, 4, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2,T,Tc",in_params=[1.01,4.99,0.55,0.74],in_upper=[15,5,10,1],in_lower=[1,1,0.01,0.01])
+		# Split into two pops, growth in one pop and two epoch in another
+		Optimize_Routine(fl, pts, prefix, "growth_twoep_sym", growth_twoep_sym_mig, rounds, 5, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2,m, T,Tc",in_params=[1.01,11.39,0.17,7.76,0.39],in_upper=[15,15,10,10,1],in_lower=[1,1,0.01,0.01,0.01])
 
-	# Split into two pops, growth in one pop and two epoch in another
-	Optimize_Routine(fl, pts, prefix, "growth_twoep_sym", growth_twoep_sym_mig, rounds, 5, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2,m, T,Tc",in_params=[1.01,11.39,0.17,7.76,0.39],in_upper=[15,15,10,10,1],in_lower=[1,1,0.01,0.01,0.01])
+		# Split into two pops, growth in one pop and two epoch in another
+		Optimize_Routine(fl, pts, prefix, "growth_twoep_asym", growth_twoep_asym_mig, rounds, 6, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2,m12,m21, T,Tc",in_params=[1.01,12.13,0.06,0.14,0.42,0.63],in_upper=[15,15,10,10,10,1],in_lower=[1,1,0.01,0.01,0.01,0.01])
+		
+		"""
+		# Split with no migration, then instantaneous size change with no migration.
+		Optimize_Routine(fs, pts, prefix, "no_mig_size", no_mig_size, rounds, 6, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1a, nu2a, nu1b, nu2b, T1, T2")
 
-	# Split into two pops, growth in one pop and two epoch in another
-	Optimize_Routine(fl, pts, prefix, "growth_twoep_asym", growth_twoep_asym_mig, rounds, 6, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2,m12,m21, T,Tc",in_params=[1.01,12.13,0.06,0.14,0.42,0.63],in_upper=[15,15,10,10,10,1],in_lower=[1,1,0.01,0.01,0.01,0.01])
-	
-	"""
-	# Split with no migration, then instantaneous size change with no migration.
-	Optimize_Routine(fs, pts, prefix, "no_mig_size", no_mig_size, rounds, 6, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1a, nu2a, nu1b, nu2b, T1, T2")
+		# Split with symmetric migration, then instantaneous size change with continuous symmetric migration.
+		Optimize_Routine(fs, pts, prefix, "sym_mig_size", sym_mig_size, rounds, 7, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1a, nu2a, nu1b, nu2b, m, T1, T2")
 
-	# Split with symmetric migration, then instantaneous size change with continuous symmetric migration.
-	Optimize_Routine(fs, pts, prefix, "sym_mig_size", sym_mig_size, rounds, 7, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1a, nu2a, nu1b, nu2b, m, T1, T2")
+		# Split with different migration rates, then instantaneous size change with continuous asymmetric migration.
+		Optimize_Routine(fs, pts, prefix, "asym_mig_size", asym_mig_size, rounds, 8, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1a, nu2a, nu1b, nu2b, m12, m21, T1, T2")
 
-	# Split with different migration rates, then instantaneous size change with continuous asymmetric migration.
-	Optimize_Routine(fs, pts, prefix, "asym_mig_size", asym_mig_size, rounds, 8, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1a, nu2a, nu1b, nu2b, m12, m21, T1, T2")
+		# Split with continuous symmetrical gene flow, followed by instantaneous size change with no migration.
+		Optimize_Routine(fs, pts, prefix, "anc_sym_mig_size", anc_sym_mig_size, rounds, 7, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1a, nu2a, nu1b, nu2b, m, T1, T2")
 
-	# Split with continuous symmetrical gene flow, followed by instantaneous size change with no migration.
-	Optimize_Routine(fs, pts, prefix, "anc_sym_mig_size", anc_sym_mig_size, rounds, 7, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1a, nu2a, nu1b, nu2b, m, T1, T2")
+		# Split with continuous asymmetrical gene flow, followed by instantaneous size change with no migration.
+		Optimize_Routine(fs, pts, prefix, "anc_asym_mig_size", anc_asym_mig_size, rounds, 8, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1a, nu2a, nu1b, nu2b, m12, m21, T1, T2")
+		"""
 
-	# Split with continuous asymmetrical gene flow, followed by instantaneous size change with no migration.
-	Optimize_Routine(fs, pts, prefix, "anc_asym_mig_size", anc_asym_mig_size, rounds, 8, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1a, nu2a, nu1b, nu2b, m12, m21, T1, T2")
-	"""
+#=================================================================================================#
+#							LOOP TO OPTIMIZE HETEROGENEOUS MODELS								  #
+#=================================================================================================#
+if heterogeneous is True:
+	pts = [ 220,240 ]
+	rounds=4
+	#define the lists for optional arguments
+	#you can change these to alter the settings of the optimization routine
+	reps = [10,20,30,40]
+	maxiters = [3,5,10,15]
+	folds = [3,2,2,1]
+	fs_folded = True
+	prefix = "flhet"
 
+	for i in range(1,2):
+		prefix = "V1_Number_{}".format(i)
+		
+		# Split into two populations, with continuous asymmetric migration.
+		Optimize_Routine(fl, pts, prefix, "asym_mig_O", asym_mig_O, rounds, 6, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, T, m12, m21, O",in_upper=[15,15,10,10,10,1],in_lower=[.1,.1,0.01,0.01,0.01,0])
+		
+		# Split into two populations, with continuous asymmetric migration.
+		Optimize_Routine(fl, pts, prefix, "asym_mig_2N", asym_mig_2N, rounds, 8, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, T, m12, m21, O, Q, hrf",in_upper=[15,15,10,10,10,1,1,1],in_lower=[.1,.1,0.01,0.01,0.01,0,0,0.0001])
 
+		# Split into two populations, with continuous asymmetric migration and genomic islands.
+		Optimize_Routine_Extrap(fl, pts, prefix, "asym_mig_2m", asym_mig_2m, rounds, 9, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, T, m12, m21, me12, me21, O, P",in_upper=[15,15,10,10,10,10,10,1,1],in_lower=[.1,.1,0.01,0.01,0.01,0.01,0.01,0,0])
+
+		# Split into two populations, with continuous asymmetric migration.
+		Optimize_Routine(fl, pts, prefix, "asym_mig_2N2m", asym_mig_2N2m, rounds, 11, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, T, m12, m21, me12, me21, O, P, Q, hrf",in_upper=[15,15,10,10,10,10,10,1,1,1,1],in_lower=[.1,.1,0.01,0.01,0.01,0.01,0.01,0,0,0,0])
---
+++ scripts/dadi_selection_mods.py
@@ -0,0 +1,210 @@
+def asym_mig(params, ns, pts):
+    """
+    Split into two populations, with different migration rates.
+    nu1: Size of population 1 after split.
+    nu2: Size of population 2 after split.
+    T: Time in the past of split (in units of 2*Na generations) 
+    m12: Migration from pop 2 to pop 1 (2*Na*m12)
+    m21: Migration from pop 1 to pop 2
+	"""
+    nu1, nu2, m12, m21, T = params
+    xx = Numerics.default_grid(pts)
+    
+    phi = PhiManip.phi_1D(xx)
+    phi = PhiManip.phi_1D_to_2D(xx, phi)
+    
+    phi = Integration.two_pops(phi, xx, T, nu1, nu2, m12=m12, m21=m21)
+    fs = Spectrum.from_phi(phi, ns, (xx,xx))
+    
+    return fs    
+
+
+	
+def asym_mig_O(params, ns, pts):
+    """
+    Split into two populations, with different migration rates.
+    nu1: Size of population 1 after split.
+    nu2: Size of population 2 after split.
+    T: Time in the past of split (in units of 2*Na generations) 
+    m12: Migration from pop 2 to pop 1 (2*Na*m12)
+    m21: Migration from pop 1 to pop 2
+	O: The proportion of accurate orientation
+	"""
+    nu1, nu2, m12, m21, T, O = params
+    xx = Numerics.default_grid(pts)
+    
+    phi = PhiManip.phi_1D(xx)
+    phi = PhiManip.phi_1D_to_2D(xx, phi)
+    
+    phi = Integration.two_pops(phi, xx, T, nu1, nu2, m12=m12, m21=m21)
+	## Calculate the spectrum for correctly oriented loci
+	fsO = Spectrum.from_phi(phi, ns, (xx,xx))
+	## Calculate the spectrum for incorrectly oriented loci
+	fsM = dadi.Numerics.reverse_array(fsO)
+	
+	# Sum the two spectra in proportion O
+    fs = O*fsO+(1-O)*fsM
+      
+    return fs    
+
+def asym_mig_2N(params, ns, pts):
+    """
+    Split into two populations, with different migration rates.
+    nu1: Size of population 1 after split.
+    nu2: Size of population 2 after split.
+    T: Time in the past of split (in units of 2*Na generations) 
+    m12: Migration from pop 2 to pop 1 (2*Na*m12)
+    m21: Migration from pop 1 to pop 2
+	O: The proportion of accurate orientation
+	Q: The proportion with reduced effective size due to selection at linked sites
+	hrf: Hill-Robertson factor, the degree to which Ne is reduced due to background selection
+	"""
+    nu1, nu2, m12, m21, T, O, Q, hrf = params
+    xx = Numerics.default_grid(pts)
+   
+	# Spectrum for normally recombining regions
+	## phi for equilibrium ancestral population
+    phinr = PhiManip.phi_1D(xx)
+	## divergence event
+    phinr = PhiManip.phi_1D_to_2D(xx, phinr)
+    ## set population sizes after split with different migration rates
+    phinr = Integration.two_pops(phinr, xx, T, nu1, nu2, m12=m12, m21=m21)
+	## Calculate the spectrum for correctly oriented loci
+    fsnrO = Spectrum.from_phi(phinr, ns, (xx,xx))
+    ## Calculate the spectrum for incorrectly oriented loci
+	fsnrM = dadi.Numerics.reverse_array(fsnrO)
+	
+	# Spectrum for low-recombining regions
+	## phi for equilibrium ancestral population
+	philr = dadi.PhiManip.phi_1D(xx)
+	## divergence event
+	philr = dadi.PhiManip.phi_1D_to_2D(xx, philr)
+	## set population sizes after split with different migration rates
+	philr = Integration.two_pops(philr, xx, T, nu1*hrf, nu2*hrf, m12=m12, m21=m21)
+	## Calculate the spectrum for correctly oriented loci
+	fslrO = Spectrum.from_phi(philr, ns, (xx,xx))
+	## Calculate the spectrum for incorrectly oriented loci
+	fslrM = dadi.Numerics.reverse_array(fslrO)
+	
+	# Sum the two spectra in proportion O
+    fs= O*(nr*fsnrO + (1-nr)*fsrO) + (1-O) *(nr*fsnrM + (1-nr)*fsrM)
+	
+    return fs   
+	
+
+def asym_mig_2m(params, ns, pts):
+    """
+    Split into two populations, with different migration rates.
+    nu1: Size of population 1 after split.
+    nu2: Size of population 2 after split.
+    T: Time in the past of split (in units of 2*Na generations) 
+    m12: Migration from pop 2 to pop 1 (2*Na*m12)
+    m21: Migration from pop 1 to pop 2
+	me12: Effective migration from pop 2 to pop 1 in genomic islands
+	me21: Effective migration from pop 1 to pop 2 in genomic islands
+	O: The proportion of accurate orientation
+	P: The proportion of the genome evolving neutrally
+	"""
+    nu1, nu2, T, m12, m21, me12, me21, O, P = params
+    xx = Numerics.default_grid(pts)
+   
+	# Spectrum for neutral spectrum
+	## phi for equilibrium ancestral population
+    phiN = PhiManip.phi_1D(xx)
+	## divergence event
+    phiN = PhiManip.phi_1D_to_2D(xx, phiN)
+    ## set population sizes after split with different migration rates
+    phiN = Integration.two_pops(phiN, xx, T, nu1, nu2, m12=m12, m21=m21)
+	## Calculate the spectrum for correctly oriented loci
+    fsNO = Spectrum.from_phi(phiN, ns, (xx,xx))
+    ## Calculate the spectrum for incorrectly oriented loci
+	fsNM = dadi.Numerics.reverse_array(fsNO)
+	
+	# Spectrum for genomic islands
+	## phi for equilibrium ancestral population
+	phiI = dadi.PhiManip.phi_1D(xx)
+	## divergence event
+	phiI = dadi.PhiManip.phi_1D_to_2D(xx, phiI)
+	## set population sizes after split with different migration rates
+	phiI = Integration.two_pops(phiI, xx, T, nu1, nu2, m12=me12, m21=me21)
+	## Calculate the spectrum for correctly oriented loci
+	fsIO = Spectrum.from_phi(phiI, ns, (xx,xx))
+	## Calculate the spectrum for incorrectly oriented loci
+	fsIM = dadi.Numerics.reverse_array(fsIO)
+	
+	# Sum the two spectra in proportion O
+    fs = O*(P*fsNO + (1-P)*fsIO) + (1-O)*(P*fsNM + (1-P)*fsIM)
+	
+    return fs   
+	
+	
+def asym_mig_2N2m(params, ns, pts):
+    """
+    Split into two populations, with different migration rates.
+    nu1: Size of population 1 after split.
+    nu2: Size of population 2 after split.
+    T: Time in the past of split (in units of 2*Na generations) 
+    m12: Migration from pop 2 to pop 1 (2*Na*m12)
+    m21: Migration from pop 1 to pop 2
+	me12: Effective migration from pop 2 to pop 1 in genomic islands
+	me21: Effective migration from pop 1 to pop 2 in genomic islands
+	O: The proportion of accurate orientation
+	P: The proportion of the genome evolving neutrally
+	Q: The proportion of the genome with reduced effective size due to linked sites
+	hrf: Hill-Robertson factor
+	"""
+    nu1, nu2, T, m12, m21, me12, me21, O, P, Q, hrf = params
+    xx = Numerics.default_grid(pts)
+   
+	# Spectrum for neutral loci
+	## phi for equilibrium ancestral population
+    phiN = PhiManip.phi_1D(xx)
+	## divergence event
+    phiN = PhiManip.phi_1D_to_2D(xx, phiN)
+    ## set population sizes after split with different migration rates
+    phiN = Integration.two_pops(phiN, xx, T, nu1, nu2, m12=m12, m21=m21)
+	## Calculate the spectrum for correctly oriented loci
+    fsNO = Spectrum.from_phi(phiN, ns, (xx,xx))
+    ## Calculate the spectrum for incorrectly oriented loci
+	fsNM = dadi.Numerics.reverse_array(fsNO)
+	
+	# Spectrum for genomic islands
+	## phi for equilibrium ancestral population
+	phiI = dadi.PhiManip.phi_1D(xx)
+	## divergence event
+	phiI = dadi.PhiManip.phi_1D_to_2D(xx, phiI)
+	## set population sizes after split with different migration rates
+	phiI = Integration.two_pops(phiI, xx, T, nu1, nu2, m12=me12, m21=me21)
+	## Calculate the spectrum for correctly oriented loci
+	fsIO = Spectrum.from_phi(phiI, ns, (xx,xx))
+	## Calculate the spectrum for incorrectly oriented loci
+	fsIM = dadi.Numerics.reverse_array(fsIO)
+	
+		# Spectrum for normally recombining regions
+	## phi for equilibrium ancestral population
+    phinr = PhiManip.phi_1D(xx)
+	## divergence event
+    phinr = PhiManip.phi_1D_to_2D(xx, phinr)
+    ## set population sizes after split with different migration rates
+    phinr = Integration.two_pops(phinr, xx, T, nu1, nu2, m12=m12, m21=m21)
+	## Calculate the spectrum for correctly oriented loci
+    fsnrO = Spectrum.from_phi(phinr, ns, (xx,xx))
+    ## Calculate the spectrum for incorrectly oriented loci
+	fsnrM = dadi.Numerics.reverse_array(fsnrO)
+	
+	# Spectrum for low-recombining regions
+	## phi for equilibrium ancestral population
+	philr = dadi.PhiManip.phi_1D(xx)
+	## divergence event
+	philr = dadi.PhiManip.phi_1D_to_2D(xx, philr)
+	## set population sizes after split with different migration rates
+	philr = Integration.two_pops(philr, xx, T, nu1*hrf, nu2*hrf, m12=m12, m21=m21)
+	## Calculate the spectrum for correctly oriented loci
+	fslrO = Spectrum.from_phi(philr, ns, (xx,xx))
+	## Calculate the spectrum for incorrectly oriented loci
+	fslrM = dadi.Numerics.reverse_array(fslrO)
+	
+	# Sum the spectra
+    fs = O*(Q*fslrO+(1-Q)*fsnrO+P*fsNO+(1-P)*fsIO) + (1-O)*(Q*fslrM+(1-Q)*fsnrM+P*fsNM+(1-P)*fsIM)
+	
+    return fs   
