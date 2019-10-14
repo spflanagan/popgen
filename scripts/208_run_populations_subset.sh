61d7bedc49427d9b1083c9d537f329db79fa99c9
spflanagan <sflanagan19@gmail.com>
2019-10-14 15:54:21 -0400

    running populations with 16 pops

--- scripts/208_run_populations_subset.sh
+++ scripts/208_run_populations_subset.sh
@@ -4,9 +4,10 @@
 RUN100=false
 RUN75=false
 RUN50=false
-RUN95=true
-RUN90=true
-RUN85=true
+RUN95=false
+RUN90=false
+RUN85=false
+RUNALL=true
 
 if [ "$RUN100" = true ]; then
 	populations -b 2 -P ./stacks -M ../fwsw_sub_map.txt -t 5 -p 7 -r 1.0 --fstats --fstats --bootstrap_fst --plink --vcf --genepop
@@ -84,4 +85,18 @@ if [ "$RUN85" = true ]; then
 	mv ./stacks/batch_2.genepop ./stacks/populations_subset85
 	mv ./stacks/batch_2.populations.log ./stacks/populations_subset85
 fi
+wait
 
+if [ "$RUNALL" = true ]; then
+	populations -b 2 -P ./stacks -M ../fwsw_pops_map.txt -t 5 -p 16 -r 0.75 --fstats --fstats --plink --vcf --genepop --treemix --structure
+	mv ./stacks/batch_2.fst* ./stacks/populations_all
+	mv ./stacks/batch_2.phistats* ./stacks/populations_all
+	mv ./stacks/batch_2.plink* ./stacks/populations_all
+	mv ./stacks/batch_2.sumstats* ./stacks/populations_all
+	mv ./stacks/batch_2.vcf ./stacks/populations_all
+	mv ./stacks/batch_2.haplotypes.tsv ./stacks/populations_all
+	mv ./stacks/batch_2.genepop ./stacks/populations_all
+	mv *batch_2*treemix* ./stacks/populations_all
+	mv *batch_2*structure* ./stacks/populations_all
+	mv ./stacks/batch_2.populations.log ./stacks/populations_all
+fi
