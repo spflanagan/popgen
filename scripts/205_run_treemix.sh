93ca65884ca6ed2a536a6f414dabbab776097989
spflanagan <spflanagan@users.noreply.github.com>
2019-10-15 14:04:23 +1300

    running treemix multiple times

--- scripts/205_run_treemix.sh
+++ scripts/205_run_treemix.sh
@@ -14,14 +14,17 @@ Rscript ../../R/vcf2treemix.R ${VCF} "poplist" "fwsw_treemix" "../../../gwscaR"
 gzip -c fwsw_treemix > fwsw_treemix.gz
 
 # run treemix
-treemix -i ${PREFIX}treemix.gz -k 100 -bootstrap -se -o ${PREFIX}k100b #without the root
-treemix -i ${PREFIX}treemix.gz -k 100 -bootstrap -root ${ROOT} -se -o ${PREFIX}k100b${ROOT}r
-treemix -i ${PREFIX}treemix.gz -k 100 -bootstrap -root ${ROOT} -se -m 1 -o ${PREFIX}k100b${ROOT}rm1
-treemix -i ${PREFIX}treemix.gz -k 100 -bootstrap -root ${ROOT} -se -m 2 -o ${PREFIX}k100b${ROOT}rm2
-treemix -i ${PREFIX}treemix.gz -k 100 -bootstrap -root ${ROOT} -se -m 3 -o ${PREFIX}k100b${ROOT}rm3
-treemix -i ${PREFIX}treemix.gz -k 100 -bootstrap -root ${ROOT} -se -m 4 -o ${PREFIX}k100b${ROOT}rm4
-treemix -i ${PREFIX}treemix.gz -k 100 -bootstrap -root ${ROOT} -se -m 5 -o ${PREFIX}k100b${ROOT}rm5
+for i in {1..10}
+do
+	treemix -i ${PREFIX}treemix.gz -k 100 -bootstrap -se -o ${PREFIX}k100b_${i} #without the root
+	treemix -i ${PREFIX}treemix.gz -k 100 -bootstrap -root ${ROOT} -se -o ${PREFIX}k100b${ROOT}r_${i}
+	treemix -i ${PREFIX}treemix.gz -k 100 -bootstrap -root ${ROOT} -se -m 1 -o ${PREFIX}k100b${ROOT}rm1_${i}
+	treemix -i ${PREFIX}treemix.gz -k 100 -bootstrap -root ${ROOT} -se -m 2 -o ${PREFIX}k100b${ROOT}rm2_${i}
+	treemix -i ${PREFIX}treemix.gz -k 100 -bootstrap -root ${ROOT} -se -m 3 -o ${PREFIX}k100b${ROOT}rm3_${i}
+	treemix -i ${PREFIX}treemix.gz -k 100 -bootstrap -root ${ROOT} -se -m 4 -o ${PREFIX}k100b${ROOT}rm4_${i}
+	treemix -i ${PREFIX}treemix.gz -k 100 -bootstrap -root ${ROOT} -se -m 5 -o ${PREFIX}k100b${ROOT}rm5_${i}
 
-# tests for treeness
-threepop -i ${PREFIX}treemix.gz -k 100 >> ${PREFIX}threepop.txt
-fourpop -i ${PREFIX}treemix.gz -k 100 >> ${PREFIX}fourpop.txt
+	# tests for treeness
+	threepop -i ${PREFIX}treemix.gz -k 100 >> ${PREFIX}threepop_${i}.txt
+	fourpop -i ${PREFIX}treemix.gz -k 100 >> ${PREFIX}fourpop_${i}.txt
+done
