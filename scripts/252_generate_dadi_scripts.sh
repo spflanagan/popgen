b3afeb6dbf2e0ffda5dfca85b1e5904d58ebca8c
spflanagan <spflanagan.phd@gmail.com>
2019-12-18 07:51:07 +1300

    allowing the dadi scripts to accept range as input

--- scripts/252_generate_dadi_scripts.sh
+++ scripts/252_generate_dadi_scripts.sh
@@ -9,8 +9,6 @@ pops=('FLLG' 'FLCC' 'ALFW' 'ALST' 'LAFW' 'TXFW' 'TXCC')
 projs=(70 60 72 70 72 46 61)
 models=('SI' 'IM' 'AM' 'SC' 'SI2N' 'SIG' 'SI2NG' 'IMG' 'IM2N' 'IM2m' 'IM2NG' 'AM2N' 'AMG' 'AM2m' 'AM2NG' 'AM2N2m' 'AM2mG' 'AM2N2mG' 'SCG' 'SC2N' 'SC2m' 'SC2NG' 'SC2N2m' 'SC2mG' 'SC2N2mG')
 
-rangeX=1
-rangeY=2
 
 ############ RUN THE ANALYSIS ############
 # the python script takes these arguments in this order:
@@ -22,7 +20,9 @@ for ((i=0; i<(${#pops[@]}-1); ++i)); do
 			echo "#!/bin/bash" > dadi_scripts/${pops[$i]}_${pops[$j]}_${models[$mod]}.sh
 			echo "cd \"${0%/*}\"" >> dadi_scripts/${pops[$i]}_${pops[$j]}_${models[$mod]}.sh
 			echo "cd ../fwsw_results/dadi_results/" >> dadi_scripts/${pops[$i]}_${pops[$j]}_${models[$mod]}.sh
-			echo "python ../../scripts/252_pairwise_dadi.py fwsw75.dadi.snps '${pops[$i]}' ${projs[$i]} '${pops[$j]}' ${projs[$j]} $rangeX $rangeY ${models[$mod]} 2>&1 > ${pops[$i]}-${pops[$j]}_${rangeX}.log" >> dadi_scripts/${pops[$i]}_${pops[$j]}_${models[$mod]}.sh
+			echo "rangeX=$1"
+			echo "rangeY=$2"
+			echo "python ../../scripts/252_pairwise_dadi.py fwsw75.dadi.snps '${pops[$i]}' ${projs[$i]} '${pops[$j]}' ${projs[$j]} ${rangeX} ${rangeY} ${models[$mod]} 2>&1 > ${pops[$i]}-${pops[$j]}_${rangeX}.log" >> dadi_scripts/${pops[$i]}_${pops[$j]}_${models[$mod]}.sh
 		done
 	done
 done
