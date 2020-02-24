8388f20c2f4ffbf6093e55e926d964a9eac8beb9
spflanagan <spflanagan.phd@gmail.com>
2020-02-24 17:16:05 +1300

    fixing up the scripts to run on the HPC

--- scripts/run_dadi_scripts_abacus.sh
+++ scripts/run_dadi_scripts_abacus.sh
@@ -9,9 +9,9 @@ cd "${0%/*}" # move to location of script
 
 
 ############ SET THESE PARAMS ############
-pops=('FLLG' 'FLCC')   #'ALFW' 'ALST' 'LAFW' 'TXFW' 'TXCC'
-models=('IM2NG' 'AM2N' 'AMG' 'AM2m' 'AM2NG' 'AM2N2m' 'AM2mG' 'AM2N2mG' 'SCG' 'SC2N' 'SC2m' 'SC2NG' 'SC2N2m' 'SC2mG' 'SC2N2mG')
-# 'SI' 'IM' 'AM' 'SC' 'SI2N' 'SIG' 'SI2NG' 'IMG' 'IM2N' 'IM2m' 
+pops=('FLLG' 'FLCC' 'ALFW' 'ALST' 'LAFW' 'TXFW' 'TXCC')   #
+models=('SI' 'IM' 'AM' 'SC' 'SI2N' 'SIG' 'SI2NG' 'IMG' 'IM2N' 'IM2m' 'IM2NG' 'AM2N' 'AMG' 'AM2m' 'AM2NG' 'AM2N2m' 'AM2mG' 'AM2N2mG' 'SCG' 'SC2N' 'SC2m' 'SC2NG' 'SC2N2m' 'SC2mG' 'SC2N2mG')
+# 
 rangeX=2
 rangeY=3
 
