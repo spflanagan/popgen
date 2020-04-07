cbfad610e18a4ef4c1237ac0399d895c0a306544
spflanagan <spflanagan.phd@gmail.com>
2020-04-07 17:47:30 +1200

    including IM2mG models

--- scripts/252_generate_dadi_scripts.sh
+++ scripts/252_generate_dadi_scripts.sh
@@ -7,7 +7,7 @@ if [ ! -d dadi_scripts ] ; then mkdir dadi_scripts ; fi
 ############ SET THESE PARAMS ############
 pops=('FLLG' 'FLCC' 'ALFW' 'ALST' 'LAFW' 'TXFW' 'TXCC')
 projs=(70 60 72 70 72 46 61)
-models=('SI' 'IM' 'AM' 'SC' 'SI2N' 'SIG' 'SI2NG' 'IMG' 'IM2N' 'IM2m' 'IM2NG' 'AM2N' 'AMG' 'AM2m' 'AM2NG' 'AM2N2m' 'AM2mG' 'AM2N2mG' 'SCG' 'SC2N' 'SC2m' 'SC2NG' 'SC2N2m' 'SC2mG' 'SC2N2mG')
+models=('SI' 'IM' 'AM' 'SC' 'SI2N' 'SIG' 'SI2NG' 'IMG' 'IM2N' 'IM2m' 'IM2mG' 'IM2NG' 'AM2N' 'AMG' 'AM2m' 'AM2NG' 'AM2N2m' 'AM2mG' 'AM2N2mG' 'SCG' 'SC2N' 'SC2m' 'SC2NG' 'SC2N2m' 'SC2mG' 'SC2N2mG')
 
 
 ############ RUN THE ANALYSIS ############
