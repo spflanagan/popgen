a899200ca68a5ce6f1302fbe41e2fde48b6f552b
spflanagan <spflanagan.phd@gmail.com>
2020-08-04 14:21:04 +1200

    using the rougemont models instead of the rougeux models which are appropriate for folded data

--- scripts/252_pairwise_dadi.py
+++ scripts/252_pairwise_dadi.py
@@ -17,7 +17,7 @@ from datetime import datetime
 #get the optimize functions
 execfile("../../programs/dadi_pipeline-master/Two_Population_Pipeline/Optimize_Functions.py")
 execfile("../../programs/dadi_pipeline-master/Two_Population_Pipeline/Models_2D.py")
-execfile("../../scripts/rougeux_models.py")
+execfile("../../scripts/rougemont_models_folded.py")
 #%load ../../programs/dadi_pipeline-master/Two_Population_Pipeline/Optimize_Functions.py
 #%load ../../programs/dadi_pipeline-master/Two_Population_Pipeline/Models_2D.py
 #%load ../../scripts/250_custom_dadi_models.py
@@ -73,68 +73,68 @@ for i in range(x,y):
 	
 	if (model=="all" or model=="SI"):
 		# Split with complete isolation
-		Optimize_Routine(spect, pts, prefix, "SI", SI, rounds, 4, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, Ts, O",in_upper=[100, 100, 10, 0.99],in_lower=[0.01, 0.01, 0, 0.01])
+		Optimize_Routine(spect, pts, prefix, "SI", SI, rounds, 3, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, Ts",in_upper=[100, 100, 10],in_lower=[0.01, 0.01, 0])
 	if (model=="all" or model=="IM"):
 		# Split into two populations with migration during divergence
-		Optimize_Routine(spect, pts, prefix, "IM", IM, rounds, 6, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, m12, m21, Ts, O",in_upper=[120, 120, 40, 40, 10, 0.99],in_lower= [0.01, 0.01, 0, 0, 0, 0.01])
+		Optimize_Routine(spect, pts, prefix, "IM", IM, rounds, 5, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, m12, m21, Ts",in_upper=[120, 120, 40, 40, 10],in_lower= [0.01, 0.01, 0, 0, 0])
 	if (model=="all" or model=="AM"):
 		# Split into two populations with ancient migration 
-		Optimize_Routine(spect, pts, prefix, "AM", AM, rounds, 7, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, m12, m21, Ts, Tam, O",in_upper=[15,15,10,10,10,10,1],in_lower=[0.1,0.1,0.1,0.01,0.1,0.1,0.00001])
+		Optimize_Routine(spect, pts, prefix, "AM", AM, rounds, 6, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, m12, m21, Ts, Tam",in_upper=[15,15,10,10,10,10],in_lower=[0.1,0.1,0.1,0.01,0.1,0.1])
 	if (model=="all" or model=="SC"):
 		# Split into two populations followed by secondary contact 
-		Optimize_Routine(spect, pts, prefix, "SC", SC, rounds, 7, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, m12, m21, Ts, Tsc, O",in_upper=[15,15,10,10,10,10,1],in_lower=[0.1,0.1,0.1,0.01,0.1,0.1,0.00001])
+		Optimize_Routine(spect, pts, prefix, "SC", SC, rounds, 6, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, m12, m21, Ts, Tsc",in_upper=[15,15,10,10,10,10],in_lower=[0.1,0.1,0.1,0.01,0.1,0.1])
 	
 	
 	#================================ HETEROGENEOUS MODELS =====================================#
 
 	# SI derivatives
 	if (model=="all" or model=="SI2N"):
-		Optimize_Routine(spect, pts, prefix, "SI2N", SI2N, rounds, 6, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, Ts, nr, bf, O",in_upper=[100, 100, 12, 1, 0.999 , 0.99],in_lower=[0.01, 0.01, 0, 0, 0.001, 0.01])
+		Optimize_Routine(spect, pts, prefix, "SI2N", SI2N, rounds, 5, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, Ts, nr, bf",in_upper=[100, 100, 12, 1, 0.999 ],in_lower=[0.01, 0.01, 0, 0, 0.001])
 	if (model=="all" or model=="SIG"):
-		Optimize_Routine(spect, pts, prefix, "SIG", SIG, rounds, 6, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, Ts, O",in_upper=[80, 80, 100, 100, 10, 0.99],in_lower=[0.01, 0.01, 0.01, 0.01, 0, 0.01])
+		Optimize_Routine(spect, pts, prefix, "SIG", SIG, rounds, 5, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, Ts",in_upper=[80, 80, 100, 100, 10],in_lower=[0.01, 0.01, 0.01, 0.01, 0])
 	if (model=="all" or model=="SI2NG"):
-		Optimize_Routine(spect, pts, prefix, "SI2NG", SI2NG, rounds, 8, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, hrf, Ts, Q, O",in_upper=[100, 100, 100, 100, 1, 10, 0.5, 0.99],in_lower=   [0.01, 0.01, 0.01, 00.1, 0.01, 0.01, 0.01, 0.01])
+		Optimize_Routine(spect, pts, prefix, "SI2NG", SI2NG, rounds, 7, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, hrf, Ts, Q",in_upper=[100, 100, 100, 100, 1, 10, 0.5],in_lower=   [0.01, 0.01, 0.01, 00.1, 0.01, 0.01, 0.01])
 	
 	# IM derivatives
 	if (model=="all" or model=="IMG"):
-		Optimize_Routine(spect, pts, prefix, "IMG", IMG, rounds, 8, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, m12, m21, Ts, O",in_upper=[120, 120, 100, 100, 40, 40, 10, 0.99],in_lower=[0.01, 0.01, 0.01, 0.01, 0, 0, 0, 0.01])
+		Optimize_Routine(spect, pts, prefix, "IMG", IMG, rounds, 7, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, m12, m21, Ts",in_upper=[120, 120, 100, 100, 40, 40, 10],in_lower=[0.01, 0.01, 0.01, 0.01, 0, 0, 0])
 	if (model=="all" or model=="IM2N"):
-		Optimize_Routine(spect, pts, prefix, "IM2N", IM2N, rounds, 8, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, hrf, m12, m21, Ts, Q, O",in_upper=[120, 120, 1, 40, 40, 10, 0.5, 0.99],in_lower=[0.01, 0.01, 0.1, 0, 0, 0, 0.01, 0.01])
+		Optimize_Routine(spect, pts, prefix, "IM2N", IM2N, rounds, 7, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, hrf, m12, m21, Ts, Q",in_upper=[120, 120, 1, 40, 40, 10, 0.5],in_lower=[0.01, 0.01, 0.1, 0, 0, 0, 0.01])
 	if (model=="all" or model=="IM2m"):
-		Optimize_Routine(spect, pts, prefix, "IM2m", IM2m, rounds, 9, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, m12, m21, me12, me21, Ts, P, O",in_upper=[250, 250, 150, 150, 45, 45, 10, 0.95, 0.99],in_lower= [0.01, 0.01, 0.0, 0.0, 0.0, 0.0, 0.0, 0.05, 0.01])
+		Optimize_Routine(spect, pts, prefix, "IM2m", IM2m, rounds, 8, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, m12, m21, me12, me21, Ts, P",in_upper=[250, 250, 150, 150, 45, 45, 10, 0.95],in_lower= [0.01, 0.01, 0.0, 0.0, 0.0, 0.0, 0.0, 0.05])
 	if (model=="all" or model=="IM2NG"):
-		Optimize_Routine(spect, pts, prefix, "IM2NG", IM2NG, rounds, 10, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, hrf, m12, m21, Ts, Q, O",in_upper=[120, 120, 80, 80, 1, 40, 40, 10, 0.5, 0.99],in_lower=[0.01, 0.01, 0.01, 0.01, 0.1, 0, 0, 0, 0.01, 0.01])
+		Optimize_Routine(spect, pts, prefix, "IM2NG", IM2NG, rounds, 9, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, hrf, m12, m21, Ts, Q",in_upper=[120, 120, 80, 80, 1, 40, 40, 10, 0.5],in_lower=[0.01, 0.01, 0.01, 0.01, 0.1, 0, 0, 0, 0.01])
 	if (model=="all" or model=="IM2mG"):
-		Optimize_Routine(spect, pts, prefix, "IM2mG", IM2mG, rounds, 11, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, m12, m21, me12, me21, Ts, P, O",in_upper=[120, 120, 200, 200, 40, 40, 30, 30, 10, 0.95, 0.99],in_lower= [0.01, 0.01, 0.01, 0.01, 0.0, 0.0, 0.0, 0.0, 0.0, 0.05, 0.01])
+		Optimize_Routine(spect, pts, prefix, "IM2mG", IM2mG, rounds, 10, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, m12, m21, me12, me21, Ts, P",in_upper=[120, 120, 200, 200, 40, 40, 30, 30, 10, 0.95],in_lower= [0.01, 0.01, 0.01, 0.01, 0.0, 0.0, 0.0, 0.0, 0.0, 0.05])
 	
 	# AM derivatives
 	if (model=="all" or model=="AM2N"):
-		Optimize_Routine(spect, pts, prefix, "AM2N", AM2N, rounds, 9, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, hrf, m12, m21, Tam, Ts, Q, O",in_upper=[80, 80, 1, 30, 30, 10, 10, 0.5, 0.99],in_lower=[0.01, 0.01, 0.1, 0, 0, 0, 0, 0.01, 0.01])
+		Optimize_Routine(spect, pts, prefix, "AM2N", AM2N, rounds, 8, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, hrf, m12, m21, Tam, Ts, Q",in_upper=[80, 80, 1, 30, 30, 10, 10, 0.5],in_lower=[0.01, 0.01, 0.1, 0, 0, 0, 0, 0.01])
 	if (model=="all" or model=="AMG"):
-		Optimize_Routine(spect, pts, prefix, "AMG", AMG, rounds, 9, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, m12, m21, Tam, Ts, O",in_upper=[120, 120, 100, 100, 30, 30, 10, 2, 0.99],in_lower=[0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0, 0, 0.01])
+		Optimize_Routine(spect, pts, prefix, "AMG", AMG, rounds, 8, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, m12, m21, Tam, Ts",in_upper=[120, 120, 100, 100, 30, 30, 10, 2],in_lower=[0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0, 0])
 	if (model=="all" or model=="AM2m"):
-		Optimize_Routine(spect, pts, prefix, "AM2m", AM2m, rounds, 10, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, m12, m21, me12, me21, Ts, Tam, P, O",in_upper=[250, 250, 150, 150, 25, 25, 10, 2, 0.95, 0.99],in_lower=[0.01, 0.01, 0, 0, 0, 0, 0, 0, 0.05, 0.01])
+		Optimize_Routine(spect, pts, prefix, "AM2m", AM2m, rounds, 9, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, m12, m21, me12, me21, Ts, Tam, P",in_upper=[250, 250, 150, 150, 25, 25, 10, 2, 0.95],in_lower=[0.01, 0.01, 0, 0, 0, 0, 0, 0, 0.05])
 	if (model=="all" or model=="AM2NG"):
-		Optimize_Routine(spect, pts, prefix, "AM2NG", AM2NG, rounds, 11, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, hrf, m12, m21, Tam, Ts, Q, O",in_upper=[90, 90, 60, 60, 1, 50, 50, 10, 10, 0.5, 0.99],in_lower=[0.01, 0.01, 0.1, 0, 0, 0, 0, 0, 0, 0.01, 0.01])
+		Optimize_Routine(spect, pts, prefix, "AM2NG", AM2NG, rounds, 10, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, hrf, m12, m21, Tam, Ts, Q",in_upper=[90, 90, 60, 60, 1, 50, 50, 10, 10, 0.5],in_lower=[0.01, 0.01, 0.1, 0, 0, 0, 0, 0, 0, 0.01])
 	if (model=="all" or model=="AM2N2m"):
-		Optimize_Routine(spect, pts, prefix, "AM2N2m", AM2N2m, rounds, 12, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, hrf, m12, m21, me12, me21, Tam, Ts, P, Q, O",in_upper=[100, 100, 1, 30, 30, 20, 20, 5, 10, 0.95, 0.5, 0.99],in_lower=[0.01, 0.01, 0.01, 0, 0, 0, 0, 0, 0, 0.05, 0.01, 0.01])
+		Optimize_Routine(spect, pts, prefix, "AM2N2m", AM2N2m, rounds, 11, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, hrf, m12, m21, me12, me21, Tam, Ts, P, Q",in_upper=[100, 100, 1, 30, 30, 20, 20, 5, 10, 0.95, 0.5],in_lower=[0.01, 0.01, 0.01, 0, 0, 0, 0, 0, 0, 0.05, 0.01])
 	if (model=="all" or model=="AM2mG"):
-		Optimize_Routine(spect, pts, prefix, "AM2mG", AM2mG, rounds, 12, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, m12, m21, me12, me21, Tam, Ts, P, O",in_upper=[90, 90, 40, 40, 30, 30, 20, 20, 2, 10, 0.95, 0.99],in_lower=[0.01, 0.01, 0.01, 0.01, 0, 0, 0, 0, 0, 0, 0.05, 0.01])
+		Optimize_Routine(spect, pts, prefix, "AM2mG", AM2mG, rounds, 11, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, m12, m21, me12, me21, Tam, Ts, P",in_upper=[90, 90, 40, 40, 30, 30, 20, 20, 2, 10, 0.95],in_lower=[0.01, 0.01, 0.01, 0.01, 0, 0, 0, 0, 0, 0, 0.05])
 	if (model=="all" or model=="AM2N2mG"):
-		Optimize_Routine(spect, pts, prefix, "AM2N2mG", AM2N2mG, rounds, 14, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, hrf, m12, m21, me12, me21, Tam, Ts, P, Q, O",in_upper=[90, 90, 50, 50, 1, 30, 30, 20, 20, 5, 10, 0.95, 0.5, 0.99],in_lower= [0.01, 0.01, 0.01, 0.01, 0.01, 0, 0, 0, 0, 0, 0, 0.05, 0.01, 0.01])
+		Optimize_Routine(spect, pts, prefix, "AM2N2mG", AM2N2mG, rounds, 13, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, hrf, m12, m21, me12, me21, Tam, Ts, P, Q",in_upper=[90, 90, 50, 50, 1, 30, 30, 20, 20, 5, 10, 0.95, 0.5],in_lower= [0.01, 0.01, 0.01, 0.01, 0.01, 0, 0, 0, 0, 0, 0, 0.05, 0.01])
 	
 	# SC derivatives
 	if (model=="all" or model=="SCG"):
-		Optimize_Routine(spect, pts, prefix, "SCG", SCG, rounds, 9, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, m12, m21, Ts, Tsc, O",in_upper=[120, 120, 150, 150, 30, 30, 10, 2, 0.99],in_lower=[0.01, 0.01, 0, 0, 0, 0, 0, 0, 0.01])
+		Optimize_Routine(spect, pts, prefix, "SCG", SCG, rounds, 8, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, m12, m21, Ts, Tsc",in_upper=[120, 120, 150, 150, 30, 30, 10, 2],in_lower=[0.01, 0.01, 0, 0, 0, 0, 0, 0])
 	if (model=="all" or model=="SC2N"):
-		Optimize_Routine(spect, pts, prefix, "SC2N", SC2N, rounds, 9, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, hrf, m12, m21, Ts, Tsc, Q, O",in_upper=[120, 120, 1, 80, 80, 10, 2, 0.5, 0.99],in_lower=[0.01, 0.01, 0.1, 0, 0, 0, 0, 0.01, 0.01])
+		Optimize_Routine(spect, pts, prefix, "SC2N", SC2N, rounds, 8, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, hrf, m12, m21, Ts, Tsc, Q",in_upper=[120, 120, 1, 80, 80, 10, 2, 0.5],in_lower=[0.01, 0.01, 0.1, 0, 0, 0, 0, 0.01])
 	if (model=="all" or model=="SC2m"):
-		Optimize_Routine(spect, pts, prefix, "SC2m", SC2m, rounds, 10, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, m12, m21, me12, me21, Ts, Tsc, P, O",in_upper=[250, 250, 150, 150, 30, 30, 10, 2, 0.95, 0.99],in_lower= [0.01, 0.01, 0, 0, 0, 0, 0, 0, 0.05, 0.01])
+		Optimize_Routine(spect, pts, prefix, "SC2m", SC2m, rounds, 9, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, m12, m21, me12, me21, Ts, Tsc, P",in_upper=[250, 250, 150, 150, 30, 30, 10, 2, 0.95],in_lower= [0.01, 0.01, 0, 0, 0, 0, 0, 0, 0.05])
 	if (model=="all" or model=="SC2NG"):
-		Optimize_Routine(spect, pts, prefix, "SC2NG", SC2NG, rounds, 11, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, hrf, m12, m21, Ts, Tsc, Q, O",in_upper=[120, 120, 150, 150, 1, 80, 80, 10, 8, 0.5, 0.99],in_lower=[0.01, 0.01, 0.01, 0.01, 0.1, 0, 0, 0, 0, 0.01, 0.01])
+		Optimize_Routine(spect, pts, prefix, "SC2NG", SC2NG, rounds, 10, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, hrf, m12, m21, Ts, Tsc, Q",in_upper=[120, 120, 150, 150, 1, 80, 80, 10, 8, 0.5],in_lower=[0.01, 0.01, 0.01, 0.01, 0.1, 0, 0, 0, 0, 0.01])
 	if (model=="all" or model=="SC2N2m"):
-		Optimize_Routine(spect, pts, prefix, "SC2N2m", SC2N2m, rounds, 12, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, hrf, m12, m21, me12, me21, Ts, Tsc, P, Q, O",in_upper=[120, 120, 1, 80, 80, 10, 10, 10, 2, 0.95, 0.5, 0.99],in_lower=[0.01, 0.01, 0.1, 0, 0, 0, 0, 0, 0, 0.5, 0.01, 0.01])
+		Optimize_Routine(spect, pts, prefix, "SC2N2m", SC2N2m, rounds, 11, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, hrf, m12, m21, me12, me21, Ts, Tsc, P, Q",in_upper=[120, 120, 1, 80, 80, 10, 10, 10, 2, 0.95, 0.5],in_lower=[0.01, 0.01, 0.1, 0, 0, 0, 0, 0, 0, 0.5, 0.01])
 	if (model=="all" or model=="SC2mG"):
-		Optimize_Routine(spect, pts, prefix, "SC2mG", SC2mG, rounds, 12, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, m12, m21, me12, me21, Ts, Tsc, P, O",in_upper=[120, 120, 150, 150, 80, 80, 10, 10, 10, 2, 0.95, 0.99],in_lower=[0.01, 0.01, 0.01, 0.01, 0, 0, 0, 0, 0, 0, 0.05, 0.01])
+		Optimize_Routine(spect, pts, prefix, "SC2mG", SC2mG, rounds, 11, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, m12, m21, me12, me21, Ts, Tsc, P",in_upper=[120, 120, 150, 150, 80, 80, 10, 10, 10, 2, 0.95],in_lower=[0.01, 0.01, 0.01, 0.01, 0, 0, 0, 0, 0, 0, 0.05])
 	if (model=="all" or model=="SC2N2mG"):
-		Optimize_Routine(spect, pts, prefix, "SC2N2mG", SC2N2mG, rounds, 14, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, hrf, m12, m21, me12, me21, Ts, Tsc, P, Q, O",in_upper= [120, 120, 200, 200, 1, 80, 80, 10, 10, 10, 5, 0.95, 0.5, 0.99],in_lower= [0.01, 0.01, 0.1, 0.01, 0.01, 0, 0, 0, 0, 0, 0, 0.5, 0.01, 0.01])
+		Optimize_Routine(spect, pts, prefix, "SC2N2mG", SC2N2mG, rounds, 13, fs_folded=fs_folded, reps=reps, maxiters=maxiters, folds=folds, param_labels = "nu1, nu2, b1, b2, hrf, m12, m21, me12, me21, Ts, Tsc, P, Q",in_upper= [120, 120, 200, 200, 1, 80, 80, 10, 10, 10, 5, 0.95, 0.5],in_lower= [0.01, 0.01, 0.1, 0.01, 0.01, 0, 0, 0, 0, 0, 0, 0.5, 0.01])
