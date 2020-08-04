b6830149b66d70a720861aabcddc36ecf4bf54e3
spflanagan <spflanagan.phd@gmail.com>
2020-08-04 14:20:17 +1200

    looping over freshwater saltwater pairs and projecting down to a lower number (30 for each pop)

--- scripts/252_generate_dadi_scripts.sh
+++ scripts/252_generate_dadi_scripts.sh
@@ -5,8 +5,10 @@ cd "${0%/*}" # move to location of script
 if [ ! -d dadi_scripts ] ; then mkdir dadi_scripts ; fi 
 
 ############ SET THESE PARAMS ############
-pops=('FLLG' 'FLCC' 'ALFW' 'ALST' 'LAFW' 'TXFW' 'TXCC')
-projs=(70 60 72 70 72 46 61)
+fw_pops=('TXFW' 'LAFW' 'ALFW' 'FLLG')
+sw_pops=('TXCC' 'ALST' 'ALST' 'FLCC')
+fw_projs=(30 30 30 30) #(54 52 60 76)
+sw_projs=(30 30 30 30) #(86 62 62 72)
 models=('SI' 'IM' 'AM' 'SC' 'SI2N' 'SIG' 'SI2NG' 'IMG' 'IM2N' 'IM2m' 'IM2mG' 'IM2NG' 'AM2N' 'AMG' 'AM2m' 'AM2NG' 'AM2N2m' 'AM2mG' 'AM2N2mG' 'SCG' 'SC2N' 'SC2m' 'SC2NG' 'SC2N2m' 'SC2mG' 'SC2N2mG')
 
 
@@ -14,16 +16,14 @@ models=('SI' 'IM' 'AM' 'SC' 'SI2N' 'SIG' 'SI2NG' 'IMG' 'IM2N' 'IM2m' 'IM2mG' 'IM
 # the python script takes these arguments in this order:
 # snps_file, popi, prji, popj, prjj, x, y
 
-for ((i=0; i<(${#pops[@]}-1); ++i)); do
-	for ((j=(i+1); j<${#pops[@]}; ++j)); do
-		for ((mod=0; mod<(${#models[@]}); ++mod)); do
-			echo "#!/bin/bash" > dadi_scripts/${pops[$i]}_${pops[$j]}_${models[$mod]}.sh
-			echo "cd \"\${0%/*}\"" >> dadi_scripts/${pops[$i]}_${pops[$j]}_${models[$mod]}.sh
-			echo "cd ../../fwsw_results/dadi_results/" >> dadi_scripts/${pops[$i]}_${pops[$j]}_${models[$mod]}.sh
-			echo "rangeX=\$1" >> dadi_scripts/${pops[$i]}_${pops[$j]}_${models[$mod]}.sh
-			echo "rangeY=\$2" >> dadi_scripts/${pops[$i]}_${pops[$j]}_${models[$mod]}.sh
-			echo "python ../../scripts/252_pairwise_dadi.py fwsw75.dadi.snps '${pops[$i]}' ${projs[$i]} '${pops[$j]}' ${projs[$j]} \$rangeX \$rangeY ${models[$mod]} 2>&1 > ${pops[$i]}_${pops[$j]}/${pops[$i]}-${pops[$j]}_${models[$mod]}_\${rangeX}_\${rangeY}.log" >> dadi_scripts/${pops[$i]}_${pops[$j]}_${models[$mod]}.sh
-		done
+for ((i=0; i<(${#fw_pops[@]}); ++i)); do
+	for ((mod=0; mod<(${#models[@]}); ++mod)); do
+		echo "#!/bin/bash" > dadi_scripts/${fw_pops[$i]}_${sw_pops[$i]}_${models[$mod]}.sh
+		echo "cd \"\${0%/*}\"" >> dadi_scripts/${fw_pops[$i]}_${sw_pops[$i]}_${models[$mod]}.sh
+		echo "cd ../../fwsw_results/dadi_results/" >> dadi_scripts/${fw_pops[$i]}_${sw_pops[$i]}_${models[$mod]}.sh
+		echo "rangeX=\$1" >> dadi_scripts/${fw_pops[$i]}_${sw_pops[$i]}_${models[$mod]}.sh
+		echo "rangeY=\$2" >> dadi_scripts/${fw_pops[$i]}_${sw_pops[$i]}_${models[$mod]}.sh
+		echo "python ../../scripts/252_pairwise_dadi.py ${fw_pops[$i]}_${sw_pops[$i]}.dadi.snps '${fw_pops[$i]}' ${fw_projs[$i]} '${sw_pops[$i]}' ${fw_projs[$i]} \$rangeX \$rangeY ${models[$mod]} 2>&1 > ${fw_pops[$i]}_${sw_pops[$i]}/${fw_pops[$i]}_${sw_pops[$i]}_${models[$mod]}_\${rangeX}_\${rangeY}.log" >> dadi_scripts/${fw_pops[$i]}_${sw_pops[$i]}_${models[$mod]}.sh
 	done
 done
 
